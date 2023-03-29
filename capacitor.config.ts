50a3ca02a1a90642eb507e964f310aee68300b83
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-03-29 13:18:00 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.podtodo',
+  appName: 'podtodo',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
