dc70971a5e9e565a3bcdadfe5bbaa4a2db88ae62
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-04-20 14:34:14 -0500

    Update useUser.ts

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -26,12 +26,6 @@ const getProfile = async () => {
 
     return data
 
-    // let { data, error, status } = await supabase
-  //       .from('profiles')
-  //       .select(`username, first_name, last_name`)
-  //       .eq('id', user?.id)
-  //       .single()
-
 }
 
 export function useUser() {
@@ -41,41 +35,3 @@ export function useUser() {
 export function useProfile() {
     return useQuery('profile', () => getProfile())
 }
-
-
-
-// const getUser = async (userId: string | undefined) => {
-//     const { data, error } = await supabase
-//         .from('profiles')
-//         .select()
-//         .eq('id', userId)
-//         .single()
-
-//     if(error) {
-//         throw new Error(error.message)
-//     }
-
-//     if(!data) {
-//         throw new Error("User not found")
-//     }
-
-//     return data
-// }
-
-// // export default async function useUser() {
-// //     // const { data } = await supabase.auth.getSession()
-// //     const { data } = await supabase.auth.getUser()
-// //     // const { data: { user } } = await supabase.auth.getUser()
-// //     return useQuery('user', async () => {
-// //         console.log('fetch user')
-// //         // return await getUser(data.session?.user.id)
-// //         return await getUser(data.user?.id)
-// //     })
-// // }
-
-// export default async function useUser() {
-//     // const { data } = await supabase.auth.getSession()
-//     const { data } = await supabase.auth.getUser()
-//     // const { data: { user } } = await supabase.auth.getUser()
-//     return useQuery('user', () => getUser(data.user?.id))
-// }
