26d74711824da9abd118437747aa9dca93dec52d
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-04-20 14:32:43 -0500

    User hook cleanup

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,38 +1,81 @@
 import { useQuery } from "react-query";
 import { supabase } from "../supabaseClient";
 
-const getUser = async (userId: string | undefined) => {
+
+const getUser = async () => {
+
+  const { data: { user } } = await supabase.auth.getUser()
+
+  return user
+
+}
+
+const getProfile = async () => {
+
+    const { data: { user } } = await supabase.auth.getUser()
+
     const { data, error } = await supabase
         .from('profiles')
-        .select()
-        .eq('id', userId)
+        .select(`username, first_name, last_name`)
+        .eq('id', user?.id)
         .single()
 
     if(error) {
         throw new Error(error.message)
     }
 
-    if(!data) {
-        throw new Error("User not found")
-    }
-
     return data
+
+    // let { data, error, status } = await supabase
+  //       .from('profiles')
+  //       .select(`username, first_name, last_name`)
+  //       .eq('id', user?.id)
+  //       .single()
+
 }
+
+export function useUser() {
+    return useQuery('user', () => getUser())
+}
+
+export function useProfile() {
+    return useQuery('profile', () => getProfile())
+}
+
+
+
+// const getUser = async (userId: string | undefined) => {
+//     const { data, error } = await supabase
+//         .from('profiles')
+//         .select()
+//         .eq('id', userId)
+//         .single()
+
+//     if(error) {
+//         throw new Error(error.message)
+//     }
+
+//     if(!data) {
+//         throw new Error("User not found")
+//     }
+
+//     return data
+// }
+
+// // export default async function useUser() {
+// //     // const { data } = await supabase.auth.getSession()
+// //     const { data } = await supabase.auth.getUser()
+// //     // const { data: { user } } = await supabase.auth.getUser()
+// //     return useQuery('user', async () => {
+// //         console.log('fetch user')
+// //         // return await getUser(data.session?.user.id)
+// //         return await getUser(data.user?.id)
+// //     })
+// // }
 
 // export default async function useUser() {
 //     // const { data } = await supabase.auth.getSession()
 //     const { data } = await supabase.auth.getUser()
 //     // const { data: { user } } = await supabase.auth.getUser()
-//     return useQuery('user', async () => {
-//         console.log('fetch user')
-//         // return await getUser(data.session?.user.id)
-//         return await getUser(data.user?.id)
-//     })
+//     return useQuery('user', () => getUser(data.user?.id))
 // }
-
-export default async function useUser() {
-    // const { data } = await supabase.auth.getSession()
-    const { data } = await supabase.auth.getUser()
-    // const { data: { user } } = await supabase.auth.getUser()
-    return useQuery('user', () => getUser(data.user?.id))
-}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,56 +2,15 @@ import { IonButton, IonContent, IonHeader, IonItem, IonPage, IonTitle, IonToolba
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-import useUser from '../hooks/useUser';
+import { useProfile, useUser } from '../hooks/useUser';
 import { useSupabase } from '../hooks/useSupabase';
-import { supabase } from '../supabaseClient';
-import { useEffect, useState } from 'react';
 
 const Home: React.FC = () => {
 
   const { logOut } = useSupabase()
-  const [session] = useState(() => supabase.auth.getSession())
 
-  const [profile, setProfile] = useState({
-    username: '',
-    first_name: '',
-    last_name: '',
-  });
-
-  const getProfile = async () => {
-    console.log('get profile');
-    const { data: { user } } = await supabase.auth.getUser()
-    try {
-      let { data, error, status } = await supabase
-        .from('profiles')
-        .select(`username, first_name, last_name`)
-        .eq('id', user?.id)
-        .single();
-      if (data) {
-        setProfile({
-          username: data.username,
-          first_name: data.first_name,
-          last_name: data.last_name,
-        })
-        console.log(profile)
-      }
-      console.log(user)
-    } catch (error: any) {
-      throw error
-    }
-  }
-
-  useEffect(() => {
-    getProfile()
-  }, [])
-
-  // const { data } = useUser()
-
-  // async function signOut() {
-  //   await logOut()
-  // }
-
-  // console.log(data)
+  const { data } = useUser()
+  const { data: profile } = useProfile()
 
 
   return (
@@ -69,7 +28,8 @@ const Home: React.FC = () => {
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
         <IonButton size="large" onClick={logOut}>Logout</IonButton>
-        <IonItem>test</IonItem>
+        <IonItem>{data?.email}</IonItem>
+        <IonItem>{profile?.first_name}</IonItem>
       </IonContent>
     </IonPage>
   );
