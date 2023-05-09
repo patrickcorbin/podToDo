798327485e3d26af0cc7c120a367f85ac2f4f69a
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-05-09 10:00:11 -0500

    Update useGetItems.ts

--- src/hooks/useGetItems.ts
+++ src/hooks/useGetItems.ts
@@ -1,19 +1,72 @@
 import { useQuery } from "react-query";
 import { supabase } from "../supabaseClient";
 
+// Queries for list items
+
 const fetchItems = async (listId: number) => {
     const { data, error } = await supabase
         .from('listItems')
         .select()
         .eq('list_id', listId)
 
-        if (error) {
-            throw new Error(error.message)
-        }
+    if (error) {
+        throw new Error(error.message)
+    }
 
-        return data
+    return data
 }
 
 export function useGetItems(listId: number) {
     return useQuery('listItems', () => fetchItems(listId))
+}
+
+const fetchItem = async (itemId: number) => {
+    const { data, error } = await supabase
+        .from('listItems')
+        .select()
+        .eq('id', itemId)
+
+    if (error) {
+        throw new Error(error.message)
+    }
+
+    return data
+}
+
+export function useGetItem(itemId: number) {
+    return useQuery('listItem', () => fetchItem(itemId))
+} 
+
+// Update item functions
+
+export const updateItem = async (itemId: number, updates: any) => {
+    const { error } = await supabase
+        .from('listItems')
+        .update(updates)
+        .eq('id', itemId)
+    
+    if (error) {
+        throw new Error(error.message)
+    }
+}
+
+export const insertItem = async (item: any) => { 
+    const { error } = await supabase
+        .from('listItems')
+        .insert(item)
+    
+    if (error) {
+        throw new Error(error.message)
+    }
+}
+
+export const deleteItem = async (itemId: number) => {
+    const { error } = await supabase
+        .from('listItems')
+        .delete()
+        .eq('id', itemId)
+
+    if (error) {
+        throw new Error(error.message)
+    }
 }
