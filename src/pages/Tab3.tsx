7d63798109329e17e42d3866d33c57499713d9e2
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-05-24 14:16:03 -0500

    Update Tab3.tsx

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,47 +1,24 @@
-import { IonContent, IonHeader, IonItem, IonPage, IonSkeletonText, IonTitle, IonToolbar, IonThumbnail, IonLabel } from '@ionic/react';
+import { IonContent, IonDatetime, IonHeader, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useEffect, useState } from 'react';
+import ListItem from '../components/ListItem';
+import { useGetItemsByDate } from '../hooks/useGetItems';
 
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
-import { supabase } from '../supabaseClient';
-import { useSupabase } from '../hooks/useSupabase';
+
 
 const Tab3: React.FC = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
 
-  const { getLists, lists } = useSupabase()
+  const [date, setDate] = useState<any>(new Date().toISOString())
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const { data, error } = await supabase
-        .from('LISTS')
-        .select()
-        if (error) {
-          throw new Error(error.message);
-        }
-        console.log(data);
-      } catch (error) {
-        // setError(error);
-        console.log(error)
-      } finally {
-        setLoading(false);
-      }
-    };
-    // fetchData();
-    getLists()
-  }, []);
-  
-  // useEffect(() => {
-  //   const getLists = async () => {
-  //     const { data, error } = await supabase
-  //       .from('LISTS')
-  //       .select()
-  // })
+  const { data: items, refetch } = useGetItemsByDate(date)
+
+  let itemDisplay = items?.map(item => (
+    <ListItem
+      key={item.id}
+      item={item}
+    />
+  ))
 
   return (
     <IonPage>
@@ -56,25 +33,14 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-        {loading && <IonItem>
-          <IonThumbnail slot="start">
-              <IonSkeletonText animated={true}></IonSkeletonText>
-            </IonThumbnail>
-            <IonLabel>
-              <h3>
-                <IonSkeletonText animated={true} style={{ 'width': '80%' }}></IonSkeletonText>
-              </h3>
-              <p>
-                <IonSkeletonText animated={true} style={{ 'width': '60%' }}></IonSkeletonText>
-              </p>
-              <p>
-                <IonSkeletonText animated={true} style={{ 'width': '30%' }}></IonSkeletonText>
-              </p>
-            </IonLabel>
-          </IonItem>
-        }
-        <h2>test 2</h2>
+        <h2>Calendar</h2>
+        <IonDatetime
+          presentation='date'
+          value={date}
+          onIonChange={(e) => setDate(e.detail.value!)}
+        >
+        </IonDatetime>
+        {itemDisplay}
       </IonContent>
     </IonPage>
   );
