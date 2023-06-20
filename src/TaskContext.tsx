b06b36b2262e6767f1929dce6e28ad7f87f3c503
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-06-20 13:56:02 -0500

    Update TaskContext.tsx

--- src/TaskContext.tsx
+++ src/TaskContext.tsx
@@ -1,10 +1,13 @@
 import { createContext, useContext, useEffect, useState } from 'react';
+import { useGetItemsAll } from './hooks/useGetItems';
 import { supabase } from './supabaseClient';
 
 interface TaskContextType {
     taskArr: any[];
     toggleIsChecked: any;
 }
+
+const { data: items } = useGetItemsAll()
 
 const TaskContext = createContext<TaskContextType>({
     taskArr: [],
@@ -18,6 +21,7 @@ export const TaskProvider = ({ children }: any) => {
     const [loading, setLoading] = useState<boolean>(true)
 
     const value = {
+        items,
         taskArr: [],
         toggleIsChecked: async () => {}
     }
